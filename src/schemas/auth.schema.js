e571193dbcf27277326d05c1939f488e8707bb2d
Jonas Veron <jonas.veron@hotmail.com>
2025-01-13 18:23:07 +0100

    add project todo-list crud WIP

---
+++ src/schemas/auth.schema.js
@@ -0,0 +1,38 @@
+import { z } from "zod"
+
+export const registerSchema = z.object({
+  username: z.string({
+    required_error: "Username is required",
+  }),
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email({
+      message: "Invalid email",
+    }),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(6, {
+      message: "Password must be a least 6 characters",
+    }),
+})
+
+export const loginSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email({
+      message: "Invalid email",
+    }),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(6, {
+      message: "Password muest be at least 6 characters",
+    }),
+})
